78fc3c6fd02f6ab25ea393e4149772c20eede599
Todd Smith <tod10784@esri.com>
2020-08-21 11:51:04 -0400

    Added FC parameter

--- TASFeatures2geojson.py
+++ TASFeatures2geojson.py
@@ -1,8 +1,11 @@
 import arcpy
 import os
 
+# Get the input feature class name.
+fc = arcpy.GetParameterAsText(0)
+
 arcpy.env.overwriteOutput = True
 arcpy.env.workspace = "C:/Users/tod10784/Documents/ArcGIS/Projects/MyProject18"
-arcpy.FeaturesToJSON_conversion(os.path.join("MyProject18.gdb", "CampusBorder"), 
-                            "C:/Work/dev/tuts/geojson/CampusBorder.geojson", 
+arcpy.FeaturesToJSON_conversion(os.path.join("MyProject18.gdb", fc), 
+                            "C:/Work/dev/tuts/geojson/" + fc + ".geojson", 
                             "NOT_FORMATTED", "Z_VALUES", "M_VALUES", "GEOJSON", "WGS84")
